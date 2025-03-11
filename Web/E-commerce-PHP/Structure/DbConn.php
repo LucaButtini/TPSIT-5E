47698091b23db2e631440a2b92180f9f8ba1c4e1
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2025-03-11 09:10:29 +0100

    Add files via upload

--- Web/E-commerce-PHP/Structure/DbConn.php
+++ Web/E-commerce-PHP/Structure/DbConn.php
@@ -1,10 +1,19 @@
 <?php
 
+require_once 'func.php';
 class DbConn
 {
     private static PDO $db;
 
-    public static function getDB():PDO{
-
+    public static function getDB($conf):PDO
+    {
+        if (!isset(self::$db)) {
+            try {
+                self::$db = new PDO($conf['dsn'], $conf['username'], $conf['password'], $conf['options']);
+            } catch (PDOException $e) {
+                logError($e);
+            }
+        }
+        return self::$db;
     }
 }
--- Web/E-commerce-PHP/Structure/db_conf.php
+++ Web/E-commerce-PHP/Structure/db_conf.php
@@ -1,4 +1,8 @@
 <?php
+
 return [
-  'dns' => ''
+    'dsn' => 'mysql:host=localhost;dbname=E_commerce',
+    'username' => 'root',
+    'password' => '',
+    'options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]
 ];
