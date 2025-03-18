0f1eb7d1e07fc3e6780f5201c5b4b7b1504de08b
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2025-03-18 08:33:36 +0100

    Delete Web/E-commerce-PHP directory

--- Web/E-commerce-PHP/Structure/func.php
+++
@@ -1,5 +0,0 @@
-<?php
-function logError(Exception $e):void{
-    error_log($e->getMessage().'---'.date('Y-m-d H:i:s'."\n"), 3,'log/database_log');
-    echo 'An error with the DB has occured'.'<br>';
-}
