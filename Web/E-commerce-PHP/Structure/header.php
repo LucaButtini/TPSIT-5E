4765215419b5a9a77be59434f4e98e8fe16f024b
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2025-03-20 10:24:06 +0100

    Update header.php

--- Web/E-commerce-PHP/Structure/header.php
+++ Web/E-commerce-PHP/Structure/header.php
@@ -9,7 +9,7 @@ $page = basename($_SERVER["SCRIPT_NAME"]);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= isset($title) ? htmlspecialchars($title) : 'E-Commerce FastRoute' ?></title>
+    <title><?= isset($title) ? htmlspecialchars($title) : 'E-Commerce' ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="Styles/style.css">
@@ -20,7 +20,7 @@ $page = basename($_SERVER["SCRIPT_NAME"]);
 
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top py-3">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><strong>E-Commerce FastRoute</strong></a>
+        <a class="navbar-brand" href="index.php"><strong>E-Commerce</strong></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
